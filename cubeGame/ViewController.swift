4780a84ef973dc6f0874fee7489aa0324913142e
Roman <romanrykhta@gmail.com>
2025-06-09 01:55:50 +0300

    Initial Commit

---
+++ cubeGame/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  cubeGame
+//
+//  Created by Roman on 09.06.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
